94d574313bec08cc70716074c8a7893042c640ba
Antonio Lau <104313812+antoniooioo@users.noreply.github.com>
2024-01-05 12:24:51 -0500

    updated social media and footer

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsInstagram } from 'react-icons/bs';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <FaLinkedin />
     </div>
     <div>
-      <FaFacebookF />
+      <FaGithub />
     </div>
     <div>
       <BsInstagram />
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -37,16 +37,16 @@ const Footer = () => {
 
   return (
     <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
+      <h2 className="head-text">chat with me</h2>
 
       <div className="app__footer-cards">
         <div className="app__footer-card ">
           <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">hello@micael.com</a>
+          <a href="mailto:antonioklau4@gmail.com" className="p-text">antonioklau4@gmail.com</a>
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+          <a href="tel:+1 (703) 209-1954" className="p-text">+1 (703) 209-1954</a>
         </div>
       </div>
       {!isFormSubmitted ? (
