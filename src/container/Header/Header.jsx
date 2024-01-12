bed976a3448962340a904bea6f58148a6ac02756
ant <104313812+antoniooioo@users.noreply.github.com>
2024-01-12 15:08:58 -0500

    updated header and logo

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -28,13 +28,16 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Micael</h1>
+            <h1 className="head-text">Ant</h1>
           </div>
         </div>
 
         <div className="tag-cmp app__flex">
           <p className="p-text">Web Developer</p>
           <p className="p-text">Freelancer</p>
+        </div>
+        <div>
+          <h1 className="head-text">That is not me -&gt;</h1>
         </div>
       </div>
     </motion.div>
